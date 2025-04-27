4d435efc77621a6050f080b6d737e5ed1b20b30f
carlos-henreis <carlos_henreis@yahoo.com.br>
2025-04-27 15:12:19 -0300

    fix Unable to handle request: '/api/badges'

--- src/app/services/badge.service.ts
+++ src/app/services/badge.service.ts
@@ -1,15 +1,23 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
 
   getBadges(): Observable<any> {
+    if (!isPlatformBrowser(this.platformId)) {
+      return of([]);
+    }
+
     return this.http.get<any>('/api/badges');
   }
 }
